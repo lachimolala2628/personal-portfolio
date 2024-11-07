a448cfa5a983e95715e8db3baebd874625c513c4
Ayush Kumar <aayushkumar1228@gmail.com>
2024-11-07 18:08:39 +0530

    changes in whatdoido

--- src/components/Whatdoi.jsx
+++ src/components/Whatdoi.jsx
@@ -11,7 +11,7 @@ const Whatdoi = () => {
                         </div>
                         <div>
                             <h3 className='font-extralight pt-10'>
-                                <span className='loaderdoido'></span> Hello! I’m Ayush Kumar, a 19-year-old Creative Frontend Developer hailing from New Delhi, India. It’s true — I’m passionate about my craft and work as a freelancer. My expertise lies in transforming designs into pixel-perfect, high-performance, accessible, and responsive websites. I thrive on the entire development process, which fuels my enthusiasm and keeps me deeply engaged in my projects. Outside of my professional life, I enjoy diving into a good book or immersing myself in music, <span className='loaderdoi'></span> which often inspires my creativity and helps me recharge.
+                                <span className='loaderdoido'></span> Hello! I’m Ayush Kumar, a 19-year-old Creative Frontend Developer hailing from New Delhi, India. It’s true — I’m passionate about my craft and work as a freelancer. My expertise lies in transforming designs into pixel-perfect, high-performance, accessible, and responsive web applications. I thrive on the entire development process, which fuels my enthusiasm and keeps me deeply engaged in my projects. Outside of my professional life, I enjoy diving into a good book or immersing myself in music, <span className='loaderdoi'></span> which often inspires my creativity and helps me recharge.
                             </h3>
                         </div>
                     </div>
