8b56887f11f356f83315581f1d3923dd7dc850ea
Ayush Kumar <aayushkumar1228@gmail.com>
2024-12-11 01:06:44 +0530

    changes in style

--- src/components/Whatdoi.jsx
+++ src/components/Whatdoi.jsx
@@ -13,7 +13,7 @@ const Whatdoi = () => {
                             <h3 className='font-extralight pt-10'>
                                 <span className='loaderdoido'></span> Hello! My name is Ayush Kumar, I’m a 19-year-old Creative Frontend Developer from New Delhi, India.  I love building high-quality, high-performance, and responsive web applications. I can turn any design into a perfect digital experience.  What I love the most that keeps the inner me engaged in the development process is the complete process from ideation to execution.
                             </h3>
-                            <h3 className='font-extralight py-10'>
+                            <h3 className='font-extralight py-8'>
                                 My work is not merely a profession; it is a craft that I take pride in. Making sure that things are accessible and optimising each detail for responsiveness and polish.
                             </h3>
 
